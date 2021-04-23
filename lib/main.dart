499bb6d5212b2bf49e3e22b75f741ed678419f38
Procoder16 <smsoumik1608@gmail.com>
2021-04-23 23:47:20 +0530

    theme changed

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.light(),
+      theme: ThemeData.dark(),
       home: InputPage(),
     );
   }
