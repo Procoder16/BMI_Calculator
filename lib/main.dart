9422f2798895ee87956684fb7c635b42409ff6b2
Procoder16 <smsoumik1608@gmail.com>
2021-04-24 22:55:22 +0530

    incorporated the dark theme with the required changes

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,9 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
+      theme: ThemeData.dark().copyWith(
         primaryColor: Color(0xFF0A0D22),
-        accentColor: Colors.grey[850],
         scaffoldBackgroundColor: Color(0xFF0A0D22),
-        textTheme: TextTheme(
-          body1: TextStyle(color: Colors.white),
-        ),
       ),
       home: InputPage(),
     );
