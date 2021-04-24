3fc3bf7f27302f44cb662bb7e9d9490da482a72a
Procoder16 <smsoumik1608@gmail.com>
2021-04-24 23:06:00 +0530

    explicitly changed the color of the floating button

--- lib/main.dart
+++ lib/main.dart
@@ -30,8 +30,11 @@ class _InputPageState extends State<InputPage> {
       body: Center(
         child: Text('Body Text'),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
+      floatingActionButton: Theme(
+        data: ThemeData.light(),
+        child: FloatingActionButton(
+          child: Icon(Icons.add),
+        ),
       ),
     );
   }
