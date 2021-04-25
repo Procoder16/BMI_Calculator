2859be9b139da8cf9908c849eb93ac573b29e655
Procoder16 <smsoumik1608@gmail.com>
2021-04-25 12:28:12 +0530

    defined the first card with the icon and the text

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -26,6 +26,25 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ReusableCard(
                     colour: cardsColour,
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          FontAwesomeIcons.mars,
+                          size: 80.0,
+                        ),
+                        SizedBox(
+                          height: 15.0,
+                        ),
+                        Text(
+                          'MALE',
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Color(0xFF8D9E98),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
                 Expanded(
