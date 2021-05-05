80250b7f353657dda7913b78d88b5b77afeaf29e
Procoder16 <smsoumik1608@gmail.com>
2021-05-05 13:55:18 +0530

    added the splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'input_page.dart';
 
@@ -12,7 +14,61 @@ class BMICalculator extends StatelessWidget {
         primaryColor: Color(0xFF11292F),
         scaffoldBackgroundColor: Color(0xFF11292F),
       ),
-      home: InputPage(),
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (_) => InputPage(),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF11292F),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'BMI CALCULATOR',
+              style: TextStyle(
+                fontFamily: 'FreckleFace',
+                fontSize: 50.0,
+                color: Color(0xFFEDE5CC),
+              ),
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            Image.asset(
+              'images/cBMI.png',
+              height: 200.0,
+            ),
+            SizedBox(
+              height: 50.0,
+            ),
+            CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Color(0xFFEDE5CC)),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
